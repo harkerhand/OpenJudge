8d32f18f8eb121d825ddb8be7667df9c80aea5d7
Harker299 <ghk20051101@outlook.com>
2023-03-12 14:38:02 +0800

    添加1973

---
+++ 1973/1973.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+long long to10(int a, int b){//a是数字，b是进制
+    long long ans = 0;
+    int temp;
+    long long x = 1;
+    while(a){
+        temp = a % 10;
+        ans += temp * x;
+        x *= b;
+        a /= 10;
+    }
+    return ans;
+}
+
+int max(int a){
+    int temp;
+    int max = 0;
+    while (a)
+    {
+        temp = a % 10;
+        a /= 10;
+        if(temp > max){
+            max = temp;
+        }
+    }
+    return max;
+}
+
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int m = 0;
+    if(max(a) > m){
+        m = max(a);
+    }
+    if(max(b) > m){
+        m = max(b);
+    }
+    if(max(c) > m){
+        m = max(c);
+    }
+
+
+    for(int i = m + 1; i; i++){
+        if(to10(a, i) * to10(b, i) == to10(c, i)){
+            cout << i;
+            break;
+        }
+        if(i == 16){
+            cout << 0;
+            break;
+        }
+    }
+    return 0;
+}
