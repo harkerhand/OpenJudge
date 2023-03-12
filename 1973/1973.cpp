d1976afd9b55e3dc15817f242cedddc022b238b6
Harker299 <ghk20051101@outlook.com>
2023-03-12 21:46:30 +0800

    666.放苹果

--- 1973/1973.cpp
+++ 1973/1973.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-long long to10(int a, int b){//a是数字，b是进制
+long long to10(int a, int b)
+{ // a是数字，b是进制
     long long ans = 0;
     int temp;
     long long x = 1;
-    while(a){
+    while (a)
+    {
         temp = a % 10;
         ans += temp * x;
         x *= b;
@@ -14,43 +16,49 @@ long long to10(int a, int b){//a是数字，b是进制
     return ans;
 }
 
-int max(int a){
+int max(int a)
+{
     int temp;
     int max = 0;
     while (a)
     {
         temp = a % 10;
         a /= 10;
-        if(temp > max){
+        if (temp > max)
+        {
             max = temp;
         }
     }
     return max;
 }
 
-
 int main()
 {
     int a, b, c;
     cin >> a >> b >> c;
     int m = 0;
-    if(max(a) > m){
+    if (max(a) > m)
+    {
         m = max(a);
     }
-    if(max(b) > m){
+    if (max(b) > m)
+    {
         m = max(b);
     }
-    if(max(c) > m){
+    if (max(c) > m)
+    {
         m = max(c);
     }
 
-
-    for(int i = m + 1; i; i++){
-        if(to10(a, i) * to10(b, i) == to10(c, i)){
+    for (int i = m + 1; i; i++)
+    {
+        if (to10(a, i) * to10(b, i) == to10(c, i))
+        {
             cout << i;
             break;
         }
-        if(i == 16){
+        if (i == 16)
+        {
             cout << 0;
             break;
         }
