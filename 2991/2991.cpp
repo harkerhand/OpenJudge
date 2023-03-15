7fe1074d2bf9c0b540c033d0964d1e43780f45b6
Harker299 <ghk20051101@outlook.com>
2023-03-15 22:58:41 +0800

    2991

--- 2991/2991.cpp
+++ 2991/2991.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+char a[201];
 
 int main()
 {
-    cout << "hello world!" << endl;
+    int k;
+    cin >> k;
+    int c;
+    while (k--)
+    {
+        cin >> a;
+        int len = strlen(a);
+        c = 1;
+        if (len >= 5)
+        {
+            int u = (a[len - 3] - '0') * 100 + (a[len - 2] - '0') * 10 + (a[len - 1] - '0');
+            u %= 500;
+            for (int i = 0; i < u; ++i)
+            {
+                c *= 2011;
+                c %= 10000;
+            }
+        }
+        else
+        {
+            int u = atof(a);
+            for (int i = 0; i < u; ++i)
+            {
+                c *= 2011;
+                c %= 10000;
+            }
+        }
+        cout << c << endl;
+    }
     return 0;
 }
